679a3fe5f0e33ef7a6231b502bc88b4af887f324
Michael Bayne <mdb@samskivert.com>
2013-04-05 17:30:05 -0700

    Starting to support Ivy depends, more work needed.

--- src/main/scala/codex/extract/ProjectModel.scala
+++ src/main/scala/codex/extract/ProjectModel.scala
@@ -67,14 +67,17 @@ object ProjectModel {
   /** Returns the appropriate project model for a project (which has known metadata). */
   def forProject (flavor :String, fqId :FqId, root :File) = flavor match {
     case "maven" => new MavenProjectModel(root)
+    case "sbt"   => new SBTProjectModel(root)
     case "m2"    => new M2ProjectModel(root, fqId)
+    case "ivy"   => new IvyProjectModel(root, fqId)
     case _       => new DefaultProjectModel(root)
   }
 
   /** Returns `Some(model)` for a dependency, or `None` if unresolvable. */
   def forDepend (depend :Depend) = (depend.flavor match {
-    case "m2" => Some(new M2ProjectModel(m2root(depend), depend.toFqId))
-    case _    => None
+    case "m2"  => Some(new M2ProjectModel(m2root(depend), depend.toFqId))
+    case "ivy" => Some(new IvyProjectModel(ivy2root(depend), depend.toFqId))
+    case _     => None
   }) flatMap(m => if (!m.isValid) None else Some(m))
 
   class DefaultProjectModel (root :File) extends ProjectModel(root) {
@@ -98,43 +101,54 @@ object ProjectModel {
   }
 
   abstract class POMProjectModel (root :File) extends ProjectModel(root) {
-    override def name       = pom.name getOrElse root.getName
-    override def groupId    = pom.groupId
-    override def artifactId = pom.artifactId
-    override def version    = pom.version
+    override def isValid    = pfile.exists
+    override def name       = _pom.name getOrElse root.getName
+    override def groupId    = _pom.groupId
+    override def artifactId = _pom.artifactId
+    override def version    = _pom.version
 
-    // TODO hack: add scala and java depends as appropriate
-    override def depends = pom.transitiveDepends(true) map { d =>
-      Depend(d.groupId, d.artifactId, d.version, "m2", d.scope == "test")
+    override def depends = {
+      val realdeps = _pom.transitiveDepends(true) map { d =>
+        Depend(d.groupId, d.artifactId, d.version, "m2", d.scope == "test")
+      }
+      // TEMP hack: add scala and java depends as appropriate
+      val haveRealJava = realdeps.exists(d => d.groupId == "java" && d.artifactId == "jdk")
+      val haveRealScala = realdeps.exists(d => d.groupId == "org.scala-lang" &&
+        d.artifactId == "scala-library")
+      val haveScalaSource = codex.file(sourceDir, "scala").exists
+      val haveJavaSource = codex.file(sourceDir, "java").exists
+      // TODO: get versions from POM
+      val fakeScalaDep = if (haveScalaSource && !haveRealScala)
+        Seq(Depend("org.scala-lang", "scala-library", "2.10.1", "m2", false)) else Seq()
+      val fakeJavaDep = if ((haveScalaSource || haveJavaSource) && !haveRealJava)
+        Seq(Depend("java", "jdk", "1.6", "m2", false)) else Seq()
+      realdeps ++ fakeScalaDep ++ fakeJavaDep
     }
 
-    protected def pom :POM
+    protected val pfile :File
+    protected lazy val _pom = POM.fromFile(pfile).get
   }
 
   class MavenProjectModel (root :File) extends POMProjectModel(root) {
     override val flavor        = "maven"
-    override def isValid       = pfile.exists
     override def isRemote      = false
-    override def sourceDir     = pom.buildProps.get("sourceDirectory") map(file(_)) getOrElse(
+    override def sourceDir     = _pom.buildProps.get("sourceDirectory") map(file(_)) getOrElse(
       file("src", "main"))
-    override def testSourceDir = pom.buildProps.get("testSourceDirectory") map(file(_)) getOrElse(
+    override def testSourceDir = _pom.buildProps.get("testSourceDirectory") map(file(_)) getOrElse(
       file("src", "test"))
 
     override def hasDocs = file("target", "site", "apidocs").exists
     override def tryGenerateDocs () = Maven.buildDocs(root)
 
-    val pfile = file("pom.xml")
-    override def pom = _pom
-    lazy val _pom = POM.fromFile(pfile).get
-
     override def needsReindex (lastIndexed :Long) =
       // TODO: should we check every subdirectory of {testS|s}ourceDir? probably...
       super.needsReindex(lastIndexed) || (pfile.lastModified > lastIndexed)
+
+    override protected val pfile = file("pom.xml")
   }
 
   class M2ProjectModel (root :File, fqId :FqId) extends POMProjectModel(root) {
     override val flavor        = "m2"
-    override def isValid       = pfile.exists
     override def isRemote      = true
     override def sourceDir     = artifact("sources")
     override def testSourceDir = root
@@ -163,9 +177,7 @@ object ProjectModel {
       }
     }
 
-    val pfile = file(s"${fqId.artifactId}-${fqId.version}.pom")
-    override def pom = _pom
-    lazy val _pom = POM.fromFile(pfile).get
+    override protected val pfile = file(s"${fqId.artifactId}-${fqId.version}.pom")
   }
 
   class SBTProjectModel (root :File) extends DefaultProjectModel(root) {
@@ -181,7 +193,18 @@ object ProjectModel {
       new File(_)) getOrElse(super.sourceDir)
     override def testSourceDir = _extracted.get("test:source-directory") map(
       new File(_)) getOrElse(super.sourceDir)
-    override def depends = _extracted.get("library-dependencies") map(SBT.parseDeps) getOrElse(Seq())
+    override def depends = {
+      val realdeps = _extracted.get("library-dependencies") map(SBT.parseDeps) getOrElse(Seq())
+      // TEMP hack: add scala and java depends as appropriate
+      val haveRealJava = realdeps.exists(d => d.groupId == "java" && d.artifactId == "jdk")
+      val haveRealScala = realdeps.exists(d => d.groupId == "org.scala-lang" &&
+        d.artifactId == "scala-library")
+      // TODO: get versions from SBT
+      val fakeScalaDep = if (!haveRealScala)
+        Seq(Depend("org.scala-lang", "scala-library", "2.10.1", "ivy", false)) else Seq()
+      val fakeJavaDep = if (!haveRealJava) Seq(Depend("java", "jdk", "1.6", "m2", false)) else Seq()
+      realdeps ++ fakeScalaDep ++ fakeJavaDep
+    }
 
     override def hasDocs = file("target", "api").exists
     override def tryGenerateDocs () = SBT.buildDocs(root)
@@ -198,8 +221,45 @@ object ProjectModel {
       "compile:source-directory", "test:source-directory")
   }
 
-  private def m2root (d :Depend) =
-    file(m2repo, Dependency(d.groupId, d.artifactId, d.version).repositoryPath :_*)
+  // TODO: revamp to read data from IVY file, not fake POM file
+  class IvyProjectModel (root :File, fqId :FqId) extends POMProjectModel(root) {
+    override val flavor        = "ivy"
+    override def isRemote      = true
+    override def sourceDir     = artifact("srcs", "sources")
+    override def testSourceDir = artifact("srcs", "test-sources") // no existy!
 
+    override def tryDownloadSource () = tryDownload("srcs", "sources")
+    override def hasDocs = artifact("docs", "javadoc").exists
+    override def tryGenerateDocs () = tryDownload("docs", "javadoc")
+
+    override def needsReindex (lastIndexed :Long) =
+      super.needsReindex(lastIndexed) || (pfile.lastModified > lastIndexed)
+
+    private def artifact (dir :String, cfier :String) =
+      file(dir, s"${fqId.artifactId}-${fqId.version}-$cfier.jar")
+
+    private def tryDownload (dir :String, cfier :String) {
+      // TODO
+      // // TODO: can we figure out what Maven repository this artifact was downloaded from?
+      // val FqId(gid, aid, vers) = fqId
+      // val gpath = gid.replace('.', '/')
+      // val url = new URL(s"http://repo2.maven.org/maven2/$gpath/$aid/$vers/$aid-$vers-$cfier.jar")
+      // log.info(s"Downloading $url...")
+      // val uconn = url.openConnection.asInstanceOf[HttpURLConnection]
+      // uconn.getResponseCode match {
+      //   case 200 => new FileOutputStream(artifact(cfier)).getChannel transferFrom(
+      //     Channels.newChannel(uconn.getInputStream), 0, Long.MaxValue)
+      //   case code => log.info(s"Download failed: $code")
+      // }
+    }
+
+    override protected val pfile = file("poms", s"${fqId.artifactId}-${fqId.version}.pom")
+  }
+
+  private def m2root (d :Depend) = file(
+    m2repo, Dependency(d.groupId, d.artifactId, d.version).repositoryPath :_*)
   private val m2repo = file(home, ".m2", "repository")
+
+  private def ivy2root (d :Depend) = file(ivy2repo, "cache", d.groupId, d.artifactId)
+  private val ivy2repo = file(home, ".ivy2")
 }
