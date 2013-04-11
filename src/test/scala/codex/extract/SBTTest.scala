a0a6da3c3883cf6fa83d82d25b818a5060aaa909
Michael Bayne <mdb@samskivert.com>
2013-04-10 17:35:51 -0700

    Wrap long line.

--- src/test/scala/codex/extract/SBTTest.scala
+++ src/test/scala/codex/extract/SBTTest.scala
@@ -12,7 +12,13 @@ import codex.data.Depend
 class SBTTest {
 
   @Test def parseDepsTest {
-    val deps = "List(org.scala-lang:scala-library:2.10.1, com.samskivert:samscala:1.0-SNAPSHOT, com.samskivert:pom-util:0.4-SNAPSHOT, org.eclipse.jetty:jetty-servlet:9.0.0.RC2, org.squeryl:squeryl_2.10:0.9.5-6, com.h2database:h2:1.2.127, junit:junit:4.8.1:test, com.novocode:junit-interface:0.7:test->default)"
+    val deps = ("List(org.scala-lang:scala-library:2.10.1, " +
+      "com.samskivert:samscala:1.0-SNAPSHOT, " +
+      "com.samskivert:pom-util:0.4-SNAPSHOT, " +
+      "org.eclipse.jetty:jetty-servlet:9.0.0.RC2, " +
+      "org.squeryl:squeryl_2.10:0.9.5-6, " +
+      "com.h2database:h2:1.2.127, junit:junit:4.8.1:test, " +
+      "com.novocode:junit-interface:0.7:test->default)")
     assertEquals(Seq(Depend("org.scala-lang", "scala-library", "2.10.1", "ivy", false),
                      Depend("com.samskivert", "samscala", "1.0-SNAPSHOT", "ivy", false),
                      Depend("com.samskivert", "pom-util", "0.4-SNAPSHOT", "ivy", false),
