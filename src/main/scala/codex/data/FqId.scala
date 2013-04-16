0b9cb2bef379f12024be34215d8732de242c5225
Michael Bayne <mdb@samskivert.com>
2013-04-16 11:07:16 -0700

    Have FqId define path to simplify putting it in a URL.

--- src/main/scala/codex/data/FqId.scala
+++ src/main/scala/codex/data/FqId.scala
@@ -12,5 +12,8 @@ case class FqId (groupId :String, artifactId :String, version :String) {
   /** Creates a copy of this FqId with `suff` tacked onto its version. */
   def dedupe (suff :Int) = copy(version = s"$version-$suff")
 
+  /** Returns a path fragment that's useful for embedding this id in a URL. */
+  def path = s"${groupId}/${artifactId}/${version}"
+
   override def toString = groupId + ":" + artifactId + ":" + version
 }
